61731e3c2bf3aec0c9136ac7d6436e76317ae029
Jhonatan Tipan <jhonatan.tipan@udla.edu.ec>
2025-04-29 07:18:40 -0500

    Agregar archivos de proyecto.

---
+++ TipanJ_ExamenProgreso1/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TipanJ_ExamenProgreso1.Models;
+
+namespace TipanJ_ExamenProgreso1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
